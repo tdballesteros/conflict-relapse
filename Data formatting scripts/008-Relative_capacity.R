8b3a8c446879b20cad2a4d472df4952f7d77867e
tdballesteros <tim.ballesteros74@yahoo.com>
2024-01-29 19:41:03 -0500

    Initial fiscal capacity script and skeleton of RPC script reformatting

--- Data formatting scripts/008-Relative_capacity.R
+++ Data formatting scripts/008-Relative_capacity.R
@@ -1,43 +1,43 @@
-# This script creates a fiscal capacity metric for each country-year based on Artbetman and Kugler's (2013) RPC.
+# This script creates a relative capacity metric for each country-year based on Artbetman and Kugler's (2013) RPC.
 
-# TODO: aid data for KSV and PSE messed up
-
-# load libraries
+### load libraries ----------------------------------------------------------------------
 library(readxl)
 library(countrycode)
 library(zoo)
 library(imputeTS)
 library(dplyr)
 
-# not in function
+### not in function ----------------------------------------------------------------------
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
-#### RPC ####
+### load data file ----------------------------------------------------------------------
 # need to fix estimates to finalised estimates
-rpc <- readxl::read_excel("~/Downloads/RPC2015_components.xlsx")
+rpc <- readxl::read_excel("Data files/Raw data files/RPC2015_components.xlsx")
 
+### format dataset ----------------------------------------------------------------------
 rpc <- rpc %>%
   dplyr::select(country,year,rpe_gdp) %>%
-  dplyr::mutate(iso3c = countrycode::countrycode(country,"country.name","iso3c")) %>%
-  dplyr::filter(iso3c %!in% c("ASM","AND","ATG","BHS","BRB","BTN","COM","DMA","FSM","GRD","KIR",
-                               "KNA","LCA","LIE","MCO","MDV","MHL","NRU","PLW","PRK","SLB","SMR",
-                               "STP","SUR","TON","TUV","VCT","VUT","WSM","ZAN","ABW","AIA","BMW",
-                               "BRN","CYM","HKG","MAC","PYF","GRL","GUM","MNE","MSR","NCL","BMU",
-                               "DJI","CUB","TKM"),
+  # fixes spelling of Guinea-Bissau
+  dplyr::mutate(country = ifelse(country=="Guinea-Bissaau","Guinea-Bissau",country),
+                # using the countrycode package, add iso3c based on country name
+                iso3c = countrycode::countrycode(country,"country.name","iso3c")) %>%
+  # remove non-sovereign entities, countries without data, and countries with very limited
+  # data (no data prior to 2000)
+  dplyr::filter(iso3c %!in% c("ASM","DMA","KIR","LCA","LIE","MCO","NRU","PRK","ABW","AIA",
+                              "BRN","CYM","HKG","MAC","PYF","GRL","GUM","MSR","NCL","BMU",
+                              "CUB","TKM"),
+                # remove non-sogereign entities that are missing from the countrycode dataset
                 country %!in% c("Faeore Islands","Netherlands Antilles"))
 
-rpc$iso3c[rpc$country=="Guinea-Bissaau"] <- "GNB"
-
-rpc <- rpc %>%
-  dplyr::select(-country)
-
+### approximate missing countries ----------------------------------------------------------------------
 # CUB approx
 # Take mean of Dominican Republic, Haiti, and Jamaica
 rpc.cub <- rpc %>%
   dplyr::filter(iso3c %in% c("HTI","DOM","JAM")) %>%
   dplyr::group_by(year) %>%
   dplyr::summarise(rpe_gdp = mean(rpe_gdp)) %>%
-  dplyr::mutate(iso3c = "CUB")
+  dplyr::mutate(iso3c = "CUB",
+                country = "Cuba")
 
 # TKM approx
 # Take mean of Kazakhstan, Kyrgyzstan, Tajikistan, and Uzbekistan
@@ -45,124 +45,148 @@ rpc.tkm <- rpc %>%
   dplyr::filter(iso3c %in% c("KAZ","KGZ","TJK","UZB")) %>%
   dplyr::group_by(year) %>%
   dplyr::summarise(rpe_gdp = mean(rpe_gdp)) %>%
-  dplyr::mutate(iso3c = "TKM")
+  dplyr::mutate(iso3c = "TKM",
+                country = "Turkmenistan")
 
+# merge estimates with main dataset
 rpc <- rpc %>%
-  rbind(rpc.cub,rpc.tkm)
+  rbind(rpc.cub,rpc.tkm) %>%
+  # reorders variables
+  dplyr::relocate(iso3c,.before=country)
 
+### expand to all country-years ----------------------------------------------------------------------
 rpc <- rpc %>%
-  dplyr::full_join(expand.grid(iso3c = unique(rpc$iso3c), year = c(1950:2019)))
+  dplyr::full_join(expand.grid(iso3c = unique(rpc$iso3c), year = c(1946:2019)))
 
-rpc.cor <- rpc %>%
-  dplyr::full_join(p5all,by=c("iso3c","year")) %>%
-  dplyr::full_join(pol,by=c("iso3c","year")) %>%
-  dplyr::full_join(gdp,by=c("iso3c","year")) %>%
-  dplyr::full_join(pd,by=c("iso3c","year")) %>%
-  dplyr::full_join(se,by=c("iso3c","year")) %>%
-  dplyr::full_join(cow,by=c("iso3c","year")) %>%
-  dplyr::full_join(cowpca1,by=c("iso3c","year")) %>%
-  dplyr::full_join(reg,by=c("iso3c")) %>%
-  #dplyr::rename(gdp = gdp.x) %>%
-  #dplyr::select(-gdp.y) %>%
-  dplyr::mutate(gdppc = gdp / pop.pd,
-                lnpop = log(pop.pd),
-                lngdppc = log(gdppc)) %>%
-  unique()
+### calculate unified/divided country data ----------------------------------------------------------------------
+# "Germany", "Yemen, Republic of" exist in dataset from 1960-2013
+# "Vietnam" begins in 1979 (post reunification)
+# post-Soviet countries begin at earliest in 1991; no USSR
+# "Serbia, Republic of" begins in 1997; no Yugoslavia
+# Czechia and Slovakia begins in 1991; no Czechoslovakia
 
-rpc.test <- glm(rpe_gdp ~ shec, data = rpc.cor)
-summary(rpc.test)
+### remove if countries did not exist that year ----------------------------------------------------------------------
+# load formatted data output by script 005-Country_years
+cyears <- read.csv("Data files/Formatted data files/country_years.csv")
 
-rpc.glm <- glm(rpe_gdp ~ parcomp + parreg + xconst + xrcomp + xropen + gdppc + lnpop + shec +
-                 lnmilexpgdp + lntroops + lnmilexpc + lnmilexpt + SSA + MENA + Americas + Europe, data = rpc.cor)
-summary(rpc.glm)
-car::vif(rpc.glm)
-
-rpc.bih.glm <- glm(rpe_gdp ~ gdppc + lnpop + shec + lnmilexpgdp + lntroops + lnmilexpc + lnmilexpt + SSA + MENA +
-                     Americas + Europe, data = rpc.cor)
-summary(rpc.bih.glm)
-
-rpc.cor <- rpc.cor %>%
-  dplyr::mutate(est = rpc.glm[["coefficients"]][1] + rpc.glm[["coefficients"]][2]*parcomp +
-                  rpc.glm[["coefficients"]][3]*parreg + rpc.glm[["coefficients"]][4]*xconst +
-                  rpc.glm[["coefficients"]][5]*xrcomp + rpc.glm[["coefficients"]][6]*xropen +
-                  rpc.glm[["coefficients"]][7]*gdppc + rpc.glm[["coefficients"]][8]*lnpop +
-                  rpc.glm[["coefficients"]][9]*shec + rpc.glm[["coefficients"]][10]*lnmilexpgdp +
-                  rpc.glm[["coefficients"]][11]*lntroops + rpc.glm[["coefficients"]][12]*lnmilexpc +
-                  rpc.glm[["coefficients"]][13]*lnmilexpt + rpc.glm[["coefficients"]][14]*SSA +
-                  rpc.glm[["coefficients"]][5]*MENA + rpc.glm[["coefficients"]][16]*Americas +
-                  rpc.glm[["coefficients"]][17]*Europe,
-                bih = rpc.bih.glm[["coefficients"]][1] + rpc.bih.glm[["coefficients"]][2]*gdppc +
-                  rpc.bih.glm[["coefficients"]][3]*lnpop + rpc.bih.glm[["coefficients"]][4]*shec +
-                  rpc.bih.glm[["coefficients"]][5]*lnmilexpgdp + rpc.bih.glm[["coefficients"]][6]*lntroops +
-                  rpc.bih.glm[["coefficients"]][7]*lnmilexpc + rpc.bih.glm[["coefficients"]][8]*lnmilexpt +
-                  rpc.bih.glm[["coefficients"]][9]*SSA + rpc.bih.glm[["coefficients"]][10]*MENA +
-                  rpc.bih.glm[["coefficients"]][11]*Americas + rpc.bih.glm[["coefficients"]][12]*Europe)
-
-plot(rpc.cor$rpe_gdp,rpc.cor$est)
-
-rpc.cor$estused[is.na(rpc.cor$rpe_gdp)&(!is.na(rpc.cor$est))] <- 1
-rpc.cor$rpe_gdp[is.na(rpc.cor$rpe_gdp)&(!is.na(rpc.cor$est))] <- rpc.cor$est[is.na(rpc.cor$rpe_gdp)&(!is.na(rpc.cor$est))]
-rpc.cor <- rpc.cor %>%
-  dplyr::filter(year > 1945)
-
+# merge datasets and filter out if the country-year is not included in the cyears dataset
 rpc <- rpc %>%
-  dplyr::filter(year < 2014)
+  dplyr::left_join(cyears,by=c("iso3c","year")) %>%
+  dplyr::filter(cn == 1) %>%
+  dplyr::select(-cn)
 
-# add estimates
-rpc_est <- read_excel("~/Documents/rpc_estimates.xlsx", sheet = 2) %>%
-  dplyr::select(-method)
+### write data ----------------------------------------------------------------------
+# writes formatted dataframe as csv files
+write.csv(rpc,"Data files/Formatted data files/relative_capacity.csv",row.names = FALSE)
 
-rpc_remove <- rpc_est %>%
-  dplyr::select(iso3c,year) %>%
-  dplyr::mutate(countryyear = paste(iso3c,year)) %>%
-  dplyr::filter(iso3c != "YEM" | year >= 1990 )
 
-rpc2 <- rpc %>%
-  dplyr::mutate(countryyear = paste(iso3c,year)) %>%
-  dplyr::filter(countryyear %!in% rpc_remove$countryyear) %>%
-  dplyr::select(-countryyear)
-
-rpc2 <- rbind(rpc2,rpc_est) %>%
-  dplyr::rename(rpc = rpe_gdp) %>%
-  dplyr::filter(year >= 1946) %>%
-  unique() %>%
-  na.omit() %>%
-  dplyr::filter(iso3c != "YEM" | year >= 1990)
-
-# remove post-Soviet and post-Yugoslav states for 1991
-rpc2 <- rpc2 %>%
-  dplyr::filter(iso3c %!in% c("ARM","AZE","BLR","EST","GEO","KAZ","KGZ","LTU","LVA","MDA","RUS",
-                              "TJK","TKM","UKR","UZB") | year >= 1992,
-           iso3c %!in% c("BIH","HRV","MKD","SRB","SVN") | year >= 1992)
-
-# rpc2 <- rpc.cor %>%
-#   dplyr::select(iso3c,year,rpe_gdp,estused) %>%
+# rpc.cor <- rpc %>%
+#   dplyr::full_join(p5all,by=c("iso3c","year")) %>%
+#   dplyr::full_join(pol,by=c("iso3c","year")) %>%
+#   dplyr::full_join(gdp,by=c("iso3c","year")) %>%
+#   dplyr::full_join(pd,by=c("iso3c","year")) %>%
+#   dplyr::full_join(se,by=c("iso3c","year")) %>%
+#   dplyr::full_join(cow,by=c("iso3c","year")) %>%
+#   dplyr::full_join(cowpca1,by=c("iso3c","year")) %>%
+#   dplyr::full_join(reg,by=c("iso3c")) %>%
+#   #dplyr::rename(gdp = gdp.x) %>%
+#   #dplyr::select(-gdp.y) %>%
+#   dplyr::mutate(gdppc = gdp / pop.pd,
+#                 lnpop = log(pop.pd),
+#                 lngdppc = log(gdppc)) %>%
+#   unique()
+# 
+# rpc.test <- glm(rpe_gdp ~ shec, data = rpc.cor)
+# summary(rpc.test)
+# 
+# rpc.glm <- glm(rpe_gdp ~ parcomp + parreg + xconst + xrcomp + xropen + gdppc + lnpop + shec +
+#                  lnmilexpgdp + lntroops + lnmilexpc + lnmilexpt + SSA + MENA + Americas + Europe, data = rpc.cor)
+# summary(rpc.glm)
+# car::vif(rpc.glm)
+# 
+# rpc.bih.glm <- glm(rpe_gdp ~ gdppc + lnpop + shec + lnmilexpgdp + lntroops + lnmilexpc + lnmilexpt + SSA + MENA +
+#                      Americas + Europe, data = rpc.cor)
+# summary(rpc.bih.glm)
+# 
+# rpc.cor <- rpc.cor %>%
+#   dplyr::mutate(est = rpc.glm[["coefficients"]][1] + rpc.glm[["coefficients"]][2]*parcomp +
+#                   rpc.glm[["coefficients"]][3]*parreg + rpc.glm[["coefficients"]][4]*xconst +
+#                   rpc.glm[["coefficients"]][5]*xrcomp + rpc.glm[["coefficients"]][6]*xropen +
+#                   rpc.glm[["coefficients"]][7]*gdppc + rpc.glm[["coefficients"]][8]*lnpop +
+#                   rpc.glm[["coefficients"]][9]*shec + rpc.glm[["coefficients"]][10]*lnmilexpgdp +
+#                   rpc.glm[["coefficients"]][11]*lntroops + rpc.glm[["coefficients"]][12]*lnmilexpc +
+#                   rpc.glm[["coefficients"]][13]*lnmilexpt + rpc.glm[["coefficients"]][14]*SSA +
+#                   rpc.glm[["coefficients"]][5]*MENA + rpc.glm[["coefficients"]][16]*Americas +
+#                   rpc.glm[["coefficients"]][17]*Europe,
+#                 bih = rpc.bih.glm[["coefficients"]][1] + rpc.bih.glm[["coefficients"]][2]*gdppc +
+#                   rpc.bih.glm[["coefficients"]][3]*lnpop + rpc.bih.glm[["coefficients"]][4]*shec +
+#                   rpc.bih.glm[["coefficients"]][5]*lnmilexpgdp + rpc.bih.glm[["coefficients"]][6]*lntroops +
+#                   rpc.bih.glm[["coefficients"]][7]*lnmilexpc + rpc.bih.glm[["coefficients"]][8]*lnmilexpt +
+#                   rpc.bih.glm[["coefficients"]][9]*SSA + rpc.bih.glm[["coefficients"]][10]*MENA +
+#                   rpc.bih.glm[["coefficients"]][11]*Americas + rpc.bih.glm[["coefficients"]][12]*Europe)
+# 
+# plot(rpc.cor$rpe_gdp,rpc.cor$est)
+# 
+# rpc.cor$estused[is.na(rpc.cor$rpe_gdp)&(!is.na(rpc.cor$est))] <- 1
+# rpc.cor$rpe_gdp[is.na(rpc.cor$rpe_gdp)&(!is.na(rpc.cor$est))] <- rpc.cor$est[is.na(rpc.cor$rpe_gdp)&(!is.na(rpc.cor$est))]
+# rpc.cor <- rpc.cor %>%
+#   dplyr::filter(year > 1945)
+# 
+# rpc <- rpc %>%
+#   dplyr::filter(year < 2014)
+# 
+# # add estimates
+# rpc_est <- read_excel("~/Documents/rpc_estimates.xlsx", sheet = 2) %>%
+#   dplyr::select(-method)
+# 
+# rpc_remove <- rpc_est %>%
+#   dplyr::select(iso3c,year) %>%
+#   dplyr::mutate(countryyear = paste(iso3c,year)) %>%
+#   dplyr::filter(iso3c != "YEM" | year >= 1990 )
+# 
+# rpc2 <- rpc %>%
+#   dplyr::mutate(countryyear = paste(iso3c,year)) %>%
+#   dplyr::filter(countryyear %!in% rpc_remove$countryyear) %>%
+#   dplyr::select(-countryyear)
+# 
+# rpc2 <- rbind(rpc2,rpc_est) %>%
+#   dplyr::rename(rpc = rpe_gdp) %>%
+#   dplyr::filter(year >= 1946) %>%
 #   unique() %>%
-#   dplyr::filter(year >= 1946)
+#   na.omit() %>%
+#   dplyr::filter(iso3c != "YEM" | year >= 1990)
 # 
-# rpc2 <- rpc2 %>%
-#   dplyr::group_by(iso3c) %>%
-#   dplyr::arrange(year) %>%
-#   dplyr::mutate(rpc_est = na_interpolation(rpe_gdp), option = "linear") %>%
-#   dplyr::ungroup() %>%
-#   select(-c(rpe_gdp,option)) %>%
-#   dplyr::rename(rpc = rpc_est)
-# 
+# # remove post-Soviet and post-Yugoslav states for 1991
 # rpc2 <- rpc2 %>%
 #   dplyr::filter(iso3c %!in% c("ARM","AZE","BLR","EST","GEO","KAZ","KGZ","LTU","LVA","MDA","RUS",
 #                               "TJK","TKM","UKR","UZB") | year >= 1992,
-#                 iso3c %!in% c("BIH","HRV","MKD","SRB","SVN") | year >= 1992)
+#            iso3c %!in% c("BIH","HRV","MKD","SRB","SVN") | year >= 1992)
 # 
-# rpc.yar <- rpc2 %>%
-#   dplyr::filter(iso3c == "YEM") %>%
-#   dplyr::mutate(iso3c = "YAR")
-# 
-# rpc.ypr <- rpc2 %>%
-#   dplyr::filter(iso3c == "YEM") %>%
-#   dplyr::mutate(iso3c = "YPR")
-# 
-# rpc2 <- rpc2 %>%
-#   rbind(rpc.yar,rpc.ypr)
-
-# writes formatted dataframe as csv files
-write.csv(rpc2,"Data files/Formatted data files/rpc.csv")
+# # rpc2 <- rpc.cor %>%
+# #   dplyr::select(iso3c,year,rpe_gdp,estused) %>%
+# #   unique() %>%
+# #   dplyr::filter(year >= 1946)
+# # 
+# # rpc2 <- rpc2 %>%
+# #   dplyr::group_by(iso3c) %>%
+# #   dplyr::arrange(year) %>%
+# #   dplyr::mutate(rpc_est = na_interpolation(rpe_gdp), option = "linear") %>%
+# #   dplyr::ungroup() %>%
+# #   select(-c(rpe_gdp,option)) %>%
+# #   dplyr::rename(rpc = rpc_est)
+# # 
+# # rpc2 <- rpc2 %>%
+# #   dplyr::filter(iso3c %!in% c("ARM","AZE","BLR","EST","GEO","KAZ","KGZ","LTU","LVA","MDA","RUS",
+# #                               "TJK","TKM","UKR","UZB") | year >= 1992,
+# #                 iso3c %!in% c("BIH","HRV","MKD","SRB","SVN") | year >= 1992)
+# # 
+# # rpc.yar <- rpc2 %>%
+# #   dplyr::filter(iso3c == "YEM") %>%
+# #   dplyr::mutate(iso3c = "YAR")
+# # 
+# # rpc.ypr <- rpc2 %>%
+# #   dplyr::filter(iso3c == "YEM") %>%
+# #   dplyr::mutate(iso3c = "YPR")
+# # 
+# # rpc2 <- rpc2 %>%
+# #   rbind(rpc.yar,rpc.ypr)
---
+++ Data formatting scripts/010-Fiscal_capacity.R
@@ -0,0 +1,197 @@
+# this script takes relative capacity and tax revenue metrics to calculate a fiscal capacity
+# metric for each country-year
+
+### load libraries ----------------------------------------------------------------------
+library(countrycode)
+library(dplyr)
+
+### load data files ----------------------------------------------------------------------
+# load formatted data output by script 007-Tax_revenue
+tax_revenue <- read.csv("Data files/Formatted data files/tax_revenue.csv")
+
+# load formatted data output by script 008-Relative_capacity
+relative_capacity <- read.csv("Data files/Formatted data files/country_years.csv")
+
+
+
+
+#### joining ####
+fiscap <- full_join(rpc2,taxrev4,by=c("iso3c","year")) %>%
+  full_join(aid2,by=c("iso3c","year")) %>%
+  full_join(cyears2,by=c("iso3c","year")) %>%
+  full_join(pd,by=c("iso3c","year")) %>%
+  filter(cn == 1 | year > 2016) %>%
+  select(-cn) %>%
+  filter(iso3c %!in% c("CPV","LUX","MLT","FSM","VUT","BHS","ATG","BRB","DMA","GRD","LCA","VCT","KNA","MCO","LIE",
+                       "AND",NA,"VAT","SMR","MNE","ZAN","COM","BTN","MDV","BRN","SLB","KIR","TUV","TON","NRU",
+                       "MHL","PLW","WSM","DJI","BLZ","FJI")) %>%
+  mutate(gdppc = gdp / pop.pd) %>%
+  mutate(lngdppc = log(gdppc)) %>%
+  mutate(aid2pc = aid2 / pop.pd) %>%
+  mutate(lnaid2pc = log(aid2pc)) %>%
+  mutate(lnaid2gdp = log(aid2gdp)) %>%
+  mutate(lntaxgdp = log(taxgdp))
+
+fiscap$aid2[is.na(fiscap$aid2)] <- 0
+fiscap$aid2[is.na(fiscap$aid2pc)] <- 0
+fiscap$aid2[is.na(fiscap$aid2gdp)] <- 0
+fiscap$lnaid2pc[fiscap$lnaid2pc==-Inf] <- 0
+fiscap$lnaid2gdp[fiscap$lnaid2gdp==-Inf] <- 0
+fiscap$lntaxgdp[fiscap$lntaxgdp==-Inf] <- 0
+
+fc.pre <- fiscap
+
+fc <- fiscap %>%
+  #  filter(year >= 1983) %>%
+  select(iso3c,year,rpc,taxgdp,aid2gdp,lntaxgdp,lnaid2gdp,aid2pc,lnaid2pc,gdppc,lngdppc) %>%
+  as.data.frame() %>%
+  mutate(countryyear = paste(iso3c,year)) %>%
+  select(-c(iso3c,year)) %>%
+  na.omit() %>%
+  unique()
+
+row.names(fc) <- fc$countryyear
+
+fc.cor <- fc %>%
+  select(-c(countryyear))
+cor(fc.cor)
+
+#### pca ####
+fcpca <- prcomp(~  rpc + taxgdp + aid2gdp,
+                data = fc, retx = T, center = T, scale. = T)
+fcpca[["rotation"]]
+
+
+cor(fc$rpc,fc$taxgdp)
+cor(fc$rpc,fc$aid2gdp)
+cor(fc$taxgdp,fc$aid2gdp)
+plot(fc$rpc,fc$taxgdp)
+
+fcpca1 <- fcpca$x[,1] %>%
+  as.data.frame()
+fcpca1 <- fcpca1 %>%
+  mutate(countryyear = row.names(fcpca1))
+names(fcpca1) <- c("pca1","countryyear")
+
+fcpca1 <- fcpca1 %>%
+  mutate(iso3c = str_sub(countryyear,start=1,end=3)) %>%
+  mutate(year = str_sub(countryyear,start=4,end=8))
+fcpca1$year <- as.numeric(fcpca1$year)
+
+fcpca1 <- fcpca1[,c("iso3c","year","pca1")] %>%
+  rename(fis.cap = pca1)
+
+#### yearly avg ####
+plot(fcpca1$year,fcpca1$fis.cap)
+
+fc.year <- fcpca1 %>%
+  group_by(year) %>%
+  summarise(avg = mean(fis.cap)) %>%
+  ungroup()
+
+plot(fc.year$year,fc.year$avg,type='l')
+
+#### ts for USSR/YUG/YEM/DEU/CZE/VNM ####
+# SOV
+arm.f <- fcpca1 %>%
+  filter(iso3c %in% c("ARM","SOV")) %>%
+  mutate(iso3c = "ARM")
+
+aze.f <- fcpca1 %>%
+  filter(iso3c %in% c("AZE","SOV")) %>%
+  mutate(iso3c = "AZE")
+
+blr.f <- fcpca1 %>%
+  filter(iso3c %in% c("BLR","SOV")) %>%
+  mutate(iso3c = "BLR")
+
+est.f <- fcpca1 %>%
+  filter(iso3c %in% c("EST","SOV")) %>%
+  mutate(iso3c = "EST")
+
+geo.f <- fcpca1 %>%
+  filter(iso3c %in% c("GEO","SOV")) %>%
+  mutate(iso3c = "GEO")
+
+ltu.f <- fcpca1 %>%
+  filter(iso3c %in% c("LTU","SOV")) %>%
+  mutate(iso3c = "LTU")
+
+lva.f <- fcpca1 %>%
+  filter(iso3c %in% c("LVA","SOV")) %>%
+  mutate(iso3c = "LVA")
+
+kaz.f <- fcpca1 %>%
+  filter(iso3c %in% c("KAZ","SOV")) %>%
+  mutate(iso3c = "KAZ")
+
+kgz.f <- fcpca1 %>%
+  filter(iso3c %in% c("KGZ","SOV")) %>%
+  mutate(iso3c = "KGZ")
+
+mda.f <- fcpca1 %>%
+  filter(iso3c %in% c("MDA","SOV")) %>%
+  mutate(iso3c = "MDA")
+
+rus.f <- fcpca1 %>%
+  filter(iso3c %in% c("RUS","SOV")) %>%
+  mutate(iso3c = "RUS")
+
+tjk.f <- fcpca1 %>%
+  filter(iso3c %in% c("TJK","SOV")) %>%
+  mutate(iso3c = "TJK")
+
+tkm.f <- fcpca1 %>%
+  filter(iso3c %in% c("TKM","SOV")) %>%
+  mutate(iso3c = "TKM")
+
+ukr.f <- fcpca1 %>%
+  filter(iso3c %in% c("UKR","SOV")) %>%
+  mutate(iso3c = "UKR")
+
+uzb.f <- fcpca1 %>%
+  filter(iso3c %in% c("UZB","SOV")) %>%
+  mutate(iso3c = "UZB")
+
+fcpca1 <- fcpca1 %>%
+  filter(iso3c %!in% c("ARM","AZE","BLR","EST","GEO","KAZ","KGZ","LTU","LVA","MDA","RUS","TJK","TKM","UKR","UZB")) %>%
+  rbind(arm.f,aze.f,blr.f,est.f,geo.f,kaz.f,kgz.f,ltu.f,lva.f,mda.f,rus.f,tjk.f,tkm.f,ukr.f,uzb.f)
+
+# 2016
+gdppop <- full_join(gdp,pd,by=c("iso3c","year")) %>%
+  mutate(gdppc = gdp / pop.pd) %>%
+  mutate(lngdppc = log(gdppc)) %>%
+  filter(year == 2016)
+
+fc.2016 <- fcpca1 %>%
+  filter(year == 2016) %>%
+  full_join(gdppop)
+
+plot(fc.2016$gdppc,fc.2016$fis.cap)
+cor(fc.2016$gdppc,fc.2016$fis.cap, use = "complete.obs")
+
+
+#### plot fc country ####
+plot.fc <- function(iso = "USA"){
+  tmp <- fcpca1 %>%
+    filter(iso3c == iso) %>%
+    arrange(year)
+  
+  plot(tmp$year,tmp$fis.cap,type='l')
+}
+
+plot(fcpca1$year,fcpca1$fis.cap)
+
+milandfis <- full_join(cowpca1,fcpca1,by=c("iso3c","year"))
+cor(milandfis$mil.cap,milandfis$fis.cap, use = "complete.obs")
+
+plot(milandfis$mil.cap,milandfis$fis.cap)
+
+#### fiscap and gdppc ####
+fvg <- fcpca1 %>%
+  left_join(gdp) %>%
+  left_join(pd) %>%
+  mutate(gdppc = gdp / pop.pd)
+
+plot(fvg$fis.cap,fvg$gdppc)
+cor(fvg$fis.cap,fvg$gdppc)
